b2dadd2f975abc02b88b4d8d76b4a4d7d8ce196c
김민철 <kmc901206@gmail.com>
2022-05-26 16:06:10 +0900

    viewPager2를 위한 fragment 5개 생성

---
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/fragments/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.neppplus.a20220526_dailyreport_retrofit.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220526_dailyreport_retrofit.R
+import com.neppplus.a20220526_dailyreport_retrofit.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment() {
+
+    lateinit var binding : FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
---
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/fragments/TimeTableFragment.kt
@@ -0,0 +1,37 @@
+package com.neppplus.a20220526_dailyreport_retrofit.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220526_dailyreport_retrofit.R
+import com.neppplus.a20220526_dailyreport_retrofit.databinding.FragmentTimeTableBinding
+
+class TimeTableFragment : BaseFragment() {
+
+    lateinit var binding : FragmentTimeTableBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_time_table, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
