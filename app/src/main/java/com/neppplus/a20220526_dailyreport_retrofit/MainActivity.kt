7a39808bb40e154eb91770f91299bde3a84dd3bf
김민철 <kmc901206@gmail.com>
2022-05-26 11:12:20 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.a20220526_dailyreport_retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
